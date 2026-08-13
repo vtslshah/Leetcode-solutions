00127af8764b4dfbcc89baf1f5e389e7bd967d99
Vatsal <vatsal@zealousweb.com>
2026-08-13 18:40:34 +0530

    LRU problem

--- lru-cache.py
+++ lru-cache.py
@@ -1,39 +1,48 @@
-class Node:
-    def __init__(self, key, val):
-        self.key, self.val = key, val
-        self.prev = self.next = None
+class Node():
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
 
 class LRUCache:
-    def __init__(self, capacity: int):
-        self.cap = capacity
-        self.cache = {}  # map key to node
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = {}
+        self.head, self.tail = Node(0,0), Node(0,0)
+        self.head.next, self.tail.prev = self.tail, self.head
 
-        self.left, self.right = Node(0, 0), Node(0, 0)
-        self.left.next, self.right.prev = self.right, self.left
+    def remove(self,node):
+        prev,next = node.prev, node.next
+        prev.next = next
+        next.prev = prev
 
-    def remove(self, node):
-        prev, next = node.prev, node.next
-        prev.next, next.prev = next, prev
-
-    def insert(self, node):
-        prev, next = self.right.prev, self.right
+    def insertNodeAtEnd(self,node):
+        prev, next = self.tail.prev, self.tail
         prev.next = next.prev = node
-        node.next, node.prev = next, prev
+        node.prev = prev
+        node.next = next
 
-    def get(self, key: int) -> int:
+    def get(self, key: int):
         if key in self.cache:
             self.remove(self.cache[key])
-            self.insert(self.cache[key])
-            return self.cache[key].val
-        return -1
+            self.insertNodeAtEnd(self.cache[key])
+            return self.cache[key].value
+        else:
+            return -1
 
-    def put(self, key: int, value: int) -> None:
+    def put(self, key:int, value:int):
         if key in self.cache:
             self.remove(self.cache[key])
+
         self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
+        self.insertNodeAtEnd(self.cache[key])
 
-        if len(self.cache) > self.cap:
-            lru = self.left.next
-            self.remove(lru)
-            del self.cache[lru.key]
+        if len(self.cache) > self.capacity:
+            LRU = self.head.next
+            self.remove(LRU)
+            del self.cache[LRU.key]
+
+
+obj = LRUCache(2)
+obj.put(1,1)
+obj.put(2,2)
+print(obj.get(1))
